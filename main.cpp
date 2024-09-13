4a37abf0469e4d50b704205aea02848960aceefb
ceiver <jagaimo20031129@gmail.com>
2024-09-13 10:13:13 +0900

    同期壁対応

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 #define CW 1 // 時計回り
 #define CCW 2 // 反時計回り
 
-// 回転扉の構造体
-struct kaiten_tobira {
-    int x, y, shape;
-    kaiten_tobira(int x, int y, const string& shape_str) : x(x), y(y) {
-        shape = stoi(shape_str, nullptr, 8); // 文字列をビット列として整数に変換
+// 回転壁の構造体
+struct kaiten_kabe {
+    int x, y, shape; // 回転壁の位置と形状
+    int M; // 同期している回転壁の数
+    vector<int> D; // 同期している回転壁のインデックス
+    kaiten_kabe(int x, int y, const string& shape_str, int M, vector<int> D) : x(x), y(y) , M(M), D(D) {
+        shape = stoi(shape_str, nullptr, 8); // 文字列を8進数として整数に変換
     }
 };
 
@@ -31,10 +33,10 @@ int get_kth_state(int key, int k, int N) {
 }
 
 // 回転可能かどうかを判定する関数
-int can_rotate(int x, int y, int dir, vector<string> &grid, vector<kaiten_tobira> &kaiten) {
+int can_rotate(int x, int y, int dir, vector<string> &grid, vector<kaiten_kabe> &kaiten) {
     int nx = x + dx[dir];
     int ny = y + dy[dir];
-    int k = grid[nx][ny] - '0'; // グリッド上の回転扉のインデックスを取得
+    int k = grid[nx][ny] - '0'; // グリッド上の回転壁のインデックスを取得
     int torque = dx[dir] * (y - kaiten[k].y) - dy[dir] * (x - kaiten[k].x); // トルクを計算
 
     if (torque == 0) return CANT; // トルクが0なら回転できない
@@ -43,20 +45,24 @@ int can_rotate(int x, int y, int dir, vector<string> &grid, vector<kaiten_tobira
 }
 
 // 次の状態のkeyを計算する関数
-int next_key(int key, int k, int rot, int N) {
-    int state = get_kth_state(key, k, N); // 現在の状態を取得
-    int next_state;
-    if (rot == CW) {
-        next_state = (state + 7) % 8; // 時計回りに回転
+int next_key(int key, int k, int rot, int N, vector<kaiten_kabe> &kaiten) {
+    int nkey = key;
+    for (auto d : kaiten[k].D) {
+        int d_state = get_kth_state(key, d, N); // 同期している回転壁の状態を取得
+        int nd_state;
+        if (rot == CW) {
+            nd_state = (d_state + 7) % 8; // 時計回りに回転
+        }
+        else {
+            nd_state = (d_state + 1) % 8; // 反時計回りに回転
+        }
+        nkey += (nd_state - d_state) * Pow8[d]; // 同期している回転壁を回転
     }
-    else {
-        next_state = (state + 1) % 8; // 反時計回りに回転
-    }
-    return key + (next_state - state) * Pow8[k]; // 新しいkeyを計算
+    return nkey; // 新しいkeyを返す
 }
 
 // 幅優先探索を行う関数
-void bfs(int sx, int sy, int gx, int gy, vector<vector<string>> &grids, vector<kaiten_tobira> &kaiten, set<int> &unable_key, vector<vector<vector<int>>> &dist, vector<vector<vector<tuple<int, int, int>>>> &par) {
+void bfs(int sx, int sy, int gx, int gy, vector<vector<string>> &grids, vector<kaiten_kabe> &kaiten, vector<bool> &is_valid, vector<vector<vector<int>>> &dist, vector<vector<vector<tuple<int, int, int>>>> &par) {
     int H = grids[0].size();
     int W = grids[0][0].size();
     int N = kaiten.size();
@@ -78,14 +84,14 @@ void bfs(int sx, int sy, int gx, int gy, vector<vector<string>> &grids, vector<k
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (!can_move(nx, ny, H, W) || grids[key][nx][ny] == '#' || grids[key][nx][ny] == '*') continue; // 移動先が範囲外または壁の場合はスキップ
-            if (grids[key][nx][ny] != '.') { // 移動先が回転扉の場合
-                int k = grids[key][nx][ny] - '0'; // 回転扉のインデックスを取得
+            if (grids[key][nx][ny] != '.') { // 移動先が回転壁の場合
+                int k = grids[key][nx][ny] - '0'; // 回転壁のインデックスを取得
                 int rot = can_rotate(x, y, i, grids[key], kaiten); // 回転可能かどうかを判定
 
                 if (rot == CANT) continue; // 回転できない場合はスキップ
 
-                nkey = next_key(key, k, rot, N); // 次の状態のkeyを計算
-                if (unable_key.count(nkey)) continue; // 無効なkeyの場合はスキップ
+                nkey = next_key(key, k, rot, N, kaiten); // 次の状態のkeyを計算
+                if (!is_valid[nkey]) continue; // 無効なkeyの場合はスキップ
             }
             
             if (dist[nkey][nx][ny] == -1) { // 移動先が空白の場合
@@ -100,29 +106,29 @@ void bfs(int sx, int sy, int gx, int gy, vector<vector<string>> &grids, vector<k
 // 8桁8進数の回転を行う関数
 int rotl8(int x, int shift) {
     int max = Pow8[8];
-    return ((x << (shift) * 3) | (x >> ((8 - shift) * 3))) % max; // 左シフトと右シフトを組み合わせて回転
+    return (x * Pow8[shift] | (x / Pow8[8 - shift])) % max; // 左シフトと右シフトを組み合わせて回転
 }
 
-// 回転扉を配置する関数
-void put_kaiten_tobira(int key, vector<string> &grid, vector<kaiten_tobira> &kaiten, set<int> &unable_key) {
+// 回転壁を配置する関数
+void put_kaiten_kabe(int key, vector<string> &grid, vector<kaiten_kabe> &kaiten, vector<bool> &is_valid) {
     int H = grid.size();
     int W = grid[0].size();
     int N = kaiten.size();
 
     for (int k = 0; k < N; k++) {
         int state = get_kth_state(key, k, N); // 現在の状態を取得
-        kaiten_tobira kt = kaiten[k];
-        int x = kt.x;
-        int y = kt.y;
-        int shape = rotl8(kt.shape, state); // 回転後の形状を計算
+        kaiten_kabe kk = kaiten[k];
+        int x = kk.x;
+        int y = kk.y;
+        int shape = rotl8(kk.shape, state); // 回転後の形状を計算
 
-        if (grid[x][y] != '.') { // 回転扉の位置に既に回転扉や壁がある場合
-            unable_key.insert(key); // 無効なkeyを記録
+        if (grid[x][y] != '.') { // 回転壁の位置に既に回転壁や壁がある場合
+            is_valid[key] = false; // 無効なkeyを記録
             return;
         }
-        grid[x][y] = '*'; // 回転扉の位置を設定
+        grid[x][y] = '*'; // 回転壁の位置を設定
         for (int i = 0; i < 8; i++) {
-            int cnt = shape / Pow8[i] % 8; // 回転扉の長さを取得
+            int cnt = shape / Pow8[i] % 8; // 回転壁の長さを取得
             for (int j = 0; j < cnt; j++) {
                 int nx = x + dx[i] * (j + 1);
                 int ny = y + dy[i] * (j + 1);
@@ -130,7 +136,7 @@ void put_kaiten_tobira(int key, vector<string> &grid, vector<kaiten_tobira> &kai
                     grid[nx][ny] = k + '0';
                 }
                 else {
-                    unable_key.insert(key); // 無効なkeyを記録
+                    is_valid[key] = false; // 無効なkeyを記録
                     return;
                 }
             }
@@ -139,10 +145,10 @@ void put_kaiten_tobira(int key, vector<string> &grid, vector<kaiten_tobira> &kai
 }
 
 // 全ての状態のグリッドを作成する関数
-void make_grids(vector<vector<string>> &grids, vector<kaiten_tobira> &kaiten, set<int> &unable_key) {
+void make_grids(vector<vector<string>> &grids, vector<kaiten_kabe> &kaiten, vector<bool> &is_valid) {
     int state_num = grids.size();
     for (int key = 0; key < state_num; key++) {
-        put_kaiten_tobira(key, grids[key], kaiten, unable_key); // 各状態に対して回転扉を配置
+        put_kaiten_kabe(key, grids[key], kaiten, is_valid); // 各状態に対して回転壁を配置
     }
 }
 
@@ -199,22 +205,28 @@ int main() {
     }
 
     int N;
-    cin >> N; // 回転扉の数を入力
-    vector<kaiten_tobira> kaiten;
+    cin >> N; // 回転壁の数を入力
+    vector<kaiten_kabe> kaiten;
     for (int i = 0; i < N; i++) {
         int x, y;
         string shape;
-        cin >> x >> y >> shape; // 回転扉の位置と形状を入力
-        kaiten.push_back(kaiten_tobira(x, y, shape));
+        cin >> x >> y >> shape; // 回転壁の位置と形状を入力
+        int M;
+        cin >> M; // 同期している回転壁の数を入力
+        vector<int> D(M);
+        for (int j = 0; j < M; j++) {
+            cin >> D[j]; // 同期している回転壁のインデックスを入力
+        }
+        kaiten.push_back(kaiten_kabe(x, y, shape, M, D)); // 回転壁を追加
     }
 
     int state_num = Pow8[N]; // 状態の数を計算
-    set<int> unable_key; // 無効なkeyを記録するセット
+    vector<bool> is_valid(state_num, true); // 有効なkeyかどうかを記録する配列
     vector<vector<string>> grids(state_num, S); // 全ての状態のグリッドを初期化
-    make_grids(grids, kaiten, unable_key); // 全ての状態のグリッドを作成
+    make_grids(grids, kaiten, is_valid); // 全ての状態のグリッドを作成
     vector<vector<vector<int>>> dist(state_num, vector<vector<int>>(H, vector<int>(W, -1))); // 距離を初期化
     vector<vector<vector<tuple<int, int, int>>>> par(state_num, vector<vector<tuple<int, int, int>>>(H, vector<tuple<int, int, int>>(W, make_tuple(-1, -1, -1)))); // 親ノードを初期化
-    bfs(sx, sy, gx, gy, grids, kaiten, unable_key, dist, par); // 幅優先探索を実行
+    bfs(sx, sy, gx, gy, grids, kaiten, is_valid, dist, par); // 幅優先探索を実行
 
     cout_result(gx, gy, grids, dist, par); // 結果を出力
 }
